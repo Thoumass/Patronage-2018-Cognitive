d71fbd2611c1d109e65a8deaeb759a8c7d7798d7
Thoumass <4990438+Thoumass@users.noreply.github.com>
2018-02-01 12:18:41 +0100

    improved working version

--- Zadanie_3/test/zadanie_3.js
+++ Zadanie_3/test/zadanie_3.js
@@ -1,5 +1,37 @@
-function hello() {
-	return 'hello';
+
+'use strict';
+
+
+function getResponse() {
+        const options = {
+            url: 'http://numbersapi.com/1',
+            headers: {
+                'User-Agent': 'request'
+            }
+        };
+
+    return new Promise ((resolve, reject) => {
+
+        get(options, function(err, resp, body) {
+            if (err) {
+                onError(err);
+                reject(err);
+            } else {
+                console.log(body);
+                resolve(body);
+            }
+        });
+    });
 }
 
-module.exports = hello;
+function onError(error) {
+    console.log('onError()', error);
+}
+
+function onPromiseResolved(arg) {
+    console.log('onPromiseResolved()', arg);
+}
+
+function onPromiseRejected(arg) {
+    console.log('onPromiseRejected()', arg);
+}
