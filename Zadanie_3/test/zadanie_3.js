cf4dcd58cca4fb3924fbda8160b52c544d68bbbc
Thoumass <4990438+Thoumass@users.noreply.github.com>
2018-02-02 13:46:29 +0100

    update

--- Zadanie_3/test/zadanie_3.js
+++ Zadanie_3/test/zadanie_3.js
@@ -1,37 +1,40 @@
 
-'use strict';
+// 'use strict';
 
 
-function getResponse() {
-        const options = {
-            url: 'http://numbersapi.com/1',
-            headers: {
-                'User-Agent': 'request'
-            }
-        };
+// function getResponse(number) {
+//         const options = {
+//             url: 'http://numbersapi.com/'+number,
+//             headers: {
+//                 'User-Agent': 'request'
+//             }
+//         };
 
-    return new Promise ((resolve, reject) => {
+//     return new Promise ((resolve, reject) => {
 
-        get(options, function(err, resp, body) {
-            if (err) {
-                onError(err);
-                reject(err);
-            } else {
-                console.log(body);
-                resolve(body);
-            }
-        });
-    });
-}
+//         get(options, function(err, resp, body) {
+//             if (err) {
+//                 onError(err);
+//                 reject(err);
+//             } else {
+//                 console.log(body);
+//                 resolve(body);
+//             }
+//         });
+//     });
+// }
 
-function onError(error) {
-    console.log('onError()', error);
-}
+// function onError(error) {
+//     console.log('onError()', error);
+// }
 
-function onPromiseResolved(arg) {
-    console.log('onPromiseResolved()', arg);
-}
+// function onPromiseResolved(arg) {
+//     console.log('onPromiseResolved()', arg);
+// }
 
-function onPromiseRejected(arg) {
-    console.log('onPromiseRejected()', arg);
-}
+// function onPromiseRejected(arg) {
+//     console.log('onPromiseRejected()', arg);
+// }
+
+// const numberPromise = getResponse(1);
+// numberPromise.then(onPromiseResolved, onPromiseRejected);
